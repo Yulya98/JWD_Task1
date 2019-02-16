73d47fdea67b9c68c1c37b8a0eb6ba3aa12385c3
YulyaKukareko <YulyaKukareko>
2019-02-16 18:26:24 +0300

    Refactoring methods work with an array

--- src/main/java/by/epam/javawebtraining/kukareko/task1/model/container/Library.java
+++ src/main/java/by/epam/javawebtraining/kukareko/task1/model/container/Library.java
@@ -3,8 +3,9 @@ package by.epam.javawebtraining.kukareko.task1.model.container;
 import by.epam.javawebtraining.kukareko.task1.logic.iterator.PublicationIterator;
 import by.epam.javawebtraining.kukareko.task1.model.entity.Publication;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
 import java.util.Iterator;
-import java.util.NoSuchElementException;
 
 /**
  * @author Yulya Kukareko
@@ -13,26 +14,21 @@ import java.util.NoSuchElementException;
 public class Library implements Iterable<Publication> {
 
     private static Publication[] publications;
-    private int index;
 
     public Library() {
         publications = new Publication[20];
-        index = 0;
     }
 
     public void add(Publication publication){
-        publications[index] = publication;
-        index++;
+        Arrays.copyOf(publications, publications.length + 1);
+        publications[publications.length - 1] = publication;
     }
 
     public void remove(int remIndex){
         for(int i = remIndex; i < publications.length; i++){
             publications[i] = publications[i + 1];
         }
-        Publication[] newPublications = new Publication[publications.length - 1];
-        System.arraycopy(publications, 0, newPublications, 0, publications.length - 1);
-        publications = newPublications;
-        index--;
+        System.arraycopy(publications, 0, publications, 0, publications.length - 1);
     }
 
     public Publication update(Publication publication){
