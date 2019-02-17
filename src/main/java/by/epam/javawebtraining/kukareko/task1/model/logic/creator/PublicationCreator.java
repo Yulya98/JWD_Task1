217fc33980ee9bc06e5c87902ac0e115012d53a4
YulyaKukareko <YulyaKukareko>
2019-02-17 11:45:54 +0300

    Refactoring class name

--- src/main/java/by/epam/javawebtraining/kukareko/task1/model/logic/creator/PublicationCreator.java
+++ src/main/java/by/epam/javawebtraining/kukareko/task1/model/logic/creator/PublicationCreator.java
@@ -17,7 +17,7 @@ import java.util.UUID;
  * @author Yulya Kukareko
  * @version 1.0 16 Feb 2019
  */
-public class PubliactionCreator {
+public class PublicationCreator {
 
     private static Random random;
     private static long index;
