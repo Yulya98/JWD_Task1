3f2ab9a6e74fded2ba1b18a0dbe3849c7774e00c
YulyaKukareko <YulyaKukareko>
2019-03-13 08:32:14 +0300

    Add diagrams

---
+++ src/main/java/by/epam/javawebtraining/kukareko/task1/model/collection/array/AbstractArrayCollection.java
@@ -0,0 +1,8 @@
+package by.epam.javawebtraining.kukareko.task1.model.collection.array;
+
+/**
+ * @author Yulya Kukareko
+ * @version 1.0 13 Март 2019
+ */
+public class AbstractArrayCollection {
+}
